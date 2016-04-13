00699a46ad733ae1c1ad4c1eaf0864ac47cfbc13
d_ojima <d_ojima@aroba.jp>
2016-04-13 23:46:23 +0900

    first commit

---
+++ SlackPostTest/SlackPostTest/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net; // 追加
+using Codeplex.Data; // 追加
+
+namespace SlackPostTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           string sendText = "";
+
+            try
+            {
+                string startupPram;
+                if (args.Length != 0)
+                {
+                    startupPram = args[0];
+
+                    if (startupPram == "/login")
+                    {
+                        sendText = "ログインシマシタ。";
+                    }
+                    else
+                    {
+                        sendText = "ログアウトシマシタ。";
+                    }
+                }
+                else
+                {
+                    sendText = "エ、ナニシタノ?";
+                }
+
+
+
+                loadSetting jsonObj = new loadSetting();
+                jsonObj.loadJson();
+                var url = jsonObj.webhookurl;
+                var username = jsonObj.username;
+                webRequest(sendText, url, username);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+
+        static bool webRequest(string sendMsg, string slackURL, string Botusername)
+        {
+            bool weRequestRes = true;
+            var wc = new WebClient();
+            var data = DynamicJson.Serialize(new
+            {
+                text = sendMsg,
+                username = Botusername
+            });
+
+            wc.Headers.Add(HttpRequestHeader.ContentType, "application/json;Charset=UTF-8");
+            wc.Encoding = Encoding.UTF8;
+
+            wc.UploadString(slackURL, data);
+
+            return weRequestRes;
+        }
+
+    }
+}
---
+++ SlackPostTest/SlackPostTest/loadSetting.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO; // 追加
+using Codeplex.Data; // 追加
+
+namespace SlackPostTest
+{
+    class loadSetting
+    {
+        // 設定ファイルとして読み込むファイル名
+        public string file = "setting.json";
+
+        // 設定をメンバで持っておく(暫定
+        public string webhookurl = "";
+        public string username = "";
+
+
+        public bool loadJson()
+        {
+            bool res = true;
+            string jsonString;
+
+            try
+            {
+                using (StreamReader reader = new StreamReader(file, Encoding.GetEncoding("UTF-8")))
+                {
+                    jsonString = reader.ReadToEnd();
+                }
+                var setting = DynamicJson.Parse(jsonString);
+                webhookurl = setting["webhookurl"];
+                username   = setting["username"];
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return res;
+        }
+    }
+}
